4828b0d75db1052831505faadc01783c44577ee1
Joshua Napoli <josh@cumulusds.com>
2019-12-30 14:56:20 -0500

    Add multi-value headers and query string parameters See https://aws.amazon.com/blogs/compute/support-for-multi-value-parameters-in-amazon-api-gateway/

--- index.js
+++ index.js
@@ -51,11 +51,13 @@ export interface CloudFrontRequestEvent {
 export type APIGatewayEvent<T = string> = {
     body: T | null;
     headers: { [name: string]: string };
+    multiValueHeaders: { [name: string]: string[] };
     httpMethod: string;
     isBase64Encoded: boolean;
     path: string;
     pathParameters: { [name: string]: string } | null;
     queryStringParameters: { [name: string]: string } | null;
+    multiValueQueryStringParameters: { [name: string]: string[] } | null;
     stageVariables: { [name: string]: string } | null;
     requestContext: {
         accountId: string;
